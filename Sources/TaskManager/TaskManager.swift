5c1761e618a41f529eb7a9ce114421c8e1a40f1b
Jay Jones <jaythejones@gmail.com>
2019-12-11 19:52:04 -0800

    Coding data

--- Sources/TaskManager/TaskManager.swift
+++ Sources/TaskManager/TaskManager.swift
@@ -2,50 +2,74 @@ import Foundation
 
 class TaskManager {
 	static let shared = TaskManager()
-	let queue = DispatchQueue(label: "Manager Queue")
-
 	let taskStore = TaskStore()
 	var taskRunner: TaskRunner?
-
+	var executingTasks = false
+	
+	enum TaskManagerError: Error {
+		case missingTaskRunner
+		case busyExecutingTasks
+	}
+	
 	func use(taskRunner: TaskRunner){
 		self.taskRunner = taskRunner
 	}
 	
-	func manage(taskRecord: TaskRecord){
-		queue.async {
-			self.taskStore.store(taskRecord)
-		}
+	func manage(task: Task){
+		self.taskStore.store(task)
 	}
 	
-	func execute(taskRecord: TaskRecord){
-		queue.async {
-			if let taskRunner = self.taskRunner {
-				taskRunner.run(taskRecord) { completedRecord in
-					self.queue.async {
-						// remove the task
-						self.taskStore.remove(completedRecord)
+	func executeTasks(completion: @escaping (Result<Bool, Error>) -> Void){
+		
+		guard let taskRunner = self.taskRunner else {
+			completion(.failure(TaskManagerError.missingTaskRunner))
+			return
+		}
+		
+		if executingTasks {
+			completion(.failure(TaskManagerError.busyExecutingTasks))
+			return
+		}
+		
+		executingTasks = true
+		let group = DispatchGroup()
+		let queue = DispatchQueue.global()
+		
+		taskStore.taskIndex.forEach { (key, task) in
+			group.enter()
+			queue.async(group: group) {
+				taskRunner.run(task: task){ result in
+					switch result {
+						case .success(_):
+							self.taskStore.remove(task) //because it should be done
+						case .failure(let error):
+							print ("Error: \(error)")
 					}
+					completion(result)
+					group.leave()
 				}
-			} else {
-				print("No taskRunner, just storing")
 			}
 		}
-	}
-	
-	func executeTasks(){
-		taskStore.taskIndex.forEach { (key, taskRecord) in
-			execute(taskRecord: taskRecord)
+		
+		let _ = group.wait(timeout: .now() + 10.0)
+		
+		group.notify(queue: queue){
+			queue.async {
+				self.executingTasks = false
+				completion(.success(true))
+			}
 		}
+
 	}
 	
+
 	// An task is an object that has an identifier and created date
-	struct TaskRecord: Codable {
+	struct Task: Codable {
 		var identifier: String
 		var createdAt: Date
-		var context: String
-//		var taskState: TaskState
-
-		init(context: String){
+		var context: Data
+		
+		init(context: Data){
 			identifier = UUID().uuidString
 			createdAt = Date()
 			self.context = context
@@ -58,7 +82,7 @@ class TaskManager {
 		}
 	}
 
-	typealias TaskIndex = [String: TaskRecord]
+	typealias TaskIndex = [String: Task]
 
 	class TaskStore {
 		var taskIndex = TaskIndex()
@@ -88,22 +112,26 @@ class TaskManager {
 		}
 		
 		
-		func url(forTask task: TaskRecord) -> URL {
+		func url(forTask task: Task) -> URL {
 			return self.baseUrl.appendingPathComponent(task.identifier + ".task")
 		}
 		
-		func store(_ record: TaskRecord){
-				
-			let fileUrl = self.url(forTask: record)
+		func store(_ task: Task){
+			if self.taskIndex[task.identifier] != nil {
+				print("Task already exists, not allowing duplicate")
+				return
+			}
+
+			let fileUrl = self.url(forTask: task)
 				
 			do {
 				let fileBaseUrl = fileUrl.deletingLastPathComponent()
 				try FileManager.default.createDirectory(at: fileBaseUrl, withIntermediateDirectories: true, attributes: nil)
-				let contentString = record.encode()
+				let contentString = task.encode()
 				try contentString.write(to: fileUrl, atomically: true, encoding: String.Encoding.utf8)
 				
-				print("TaskStore saved \(record) to file \n\(fileUrl)")
-				self.taskIndex[record.identifier] = record
+				print("TaskStore saved \(task) to file \n\(fileUrl)")
+				self.taskIndex[task.identifier] = task
 				self.saveIndex()
 			} catch {
 				// failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
@@ -111,53 +139,59 @@ class TaskManager {
 			}
 		}
 		
-		func remove(_ record: TaskRecord){
-			let fileUrl = self.url(forTask: record)
+		func remove(_ task: Task){
+			let fileUrl = self.url(forTask: task)
 			print("Removing \(fileUrl)")
-			self.taskIndex.removeValue(forKey: record.identifier)
+			self.taskIndex.removeValue(forKey: task.identifier)
 			try! FileManager.default.removeItem(at: fileUrl)
 		}
 		
 		func loadIndex() {
-			 do {
-
-				 print ("TaskStore")
-				 
-				 if let cachePath = cachePath() {
-					 let url = URL(fileURLWithPath: cachePath)
-					 let d = try Data(contentsOf:url)
-					 
-					 let decoder = JSONDecoder()
-					 self.taskIndex = try decoder.decode(TaskIndex.self, from: d)
-				 }
-				 else {
-					 print("Failed to unarchive task index")
-				 }
-			 } catch {
-				 print("Failed to decode task index")
-			 }
-		 }
-		 
-		 func saveIndex(){
-			 do {
-				 if let cachePath = cachePath() {
-					 let url = URL(fileURLWithPath: cachePath)
-
-					 let encoder = JSONEncoder()
-					 let d = try encoder.encode(self.taskIndex)
-					 try d.write(to: url)
-					 print("Saved cache index \(url)")
-				 } else {
-					 print("Failed to encode index")
-				 }
-			 } catch {
-				 print("Failed to save index")
-			 }
-		 }
+			do {
+				if let cachePath = cachePath() {
+					let url = URL(fileURLWithPath: cachePath)
+					let d = try Data(contentsOf:url)
+					
+					let decoder = JSONDecoder()
+					self.taskIndex = try decoder.decode(TaskIndex.self, from: d)
+				}
+				else {
+					print("Failed to unarchive task index")
+				}
+			} catch {
+				print("Failed to decode task index")
+			}
+		}
+		
+		func saveIndex(){
+			do {
+				if let cachePath = cachePath() {
+					let url = URL(fileURLWithPath: cachePath)
+					
+					let encoder = JSONEncoder()
+					let d = try encoder.encode(self.taskIndex)
+					try d.write(to: url)
+					print("Saved cache index \(url)")
+				} else {
+					print("Failed to encode index")
+				}
+			} catch {
+				print("Failed to save index")
+			}
+		}
+		
+		func clear(){
+			if (FileManager.default.fileExists(atPath: self.baseUrl.path)){
+				do { try FileManager.default.removeItem(at: self.baseUrl) }
+				catch { print("Failed to delete \(self.baseUrl.path), maybe it didn't exist yet?") }
+			}
+			self.taskIndex.removeAll()
+		}
 	}
 }
 
 
 protocol TaskRunner {
-	func run(_ taskRecord: TaskManager.TaskRecord, completion: @escaping (TaskManager.TaskRecord) -> Void)
+	func run(task: TaskManager.Task, completion: @escaping (Result<Bool, Error>) -> Void)
 }
+
--- Tests/TaskManagerTests/TaskManagerTests.swift
+++ Tests/TaskManagerTests/TaskManagerTests.swift
@@ -3,12 +3,27 @@ import XCTest
 
 final class TaskManagerTests: XCTestCase {
     func testManagement() {
-		TaskManager.shared.use(taskRunner: TestTaskRunner())
-		let testContext = TestTaskContext()
-		let taskRecord = TaskManager.TaskRecord(context: testContext.encode())
+		TaskManager.shared.taskStore.clear()
 
-		TaskManager.shared.manage(taskRecord: taskRecord)
-		XCTAssert(true)
+		TaskManager.shared.use(taskRunner: TestTaskRunner())
+		let customAction = TestAction()
+		let context = customAction.encode()
+		let task = TaskManager.Task(context: context)
+//		let c2 = TestAction.decode(from: context)
+		
+		TaskManager.shared.manage(task: task)
+		TaskManager.shared.manage(task: task)
+		// Size of managment queue should only be 1 (no duplicates)
+		XCTAssertEqual(TaskManager.shared.taskStore.taskIndex.count, 1)
+		
+//		TaskManager.shared.manage(task: TaskManager.Task(context: "here's my custom context"))
+		
+		TaskManager.shared.executeTasks { (result) in
+			print("Finished tasks with result: \(result)")
+		}
+//		TaskManager.shared.taskStore.clear()
+//		XCTAssertEqual(TaskManager.shared.taskStore.taskIndex.count, 0)
+
     }
 
     static var allTests = [
@@ -16,19 +31,24 @@ final class TaskManagerTests: XCTestCase {
     ]
 }
 
-struct TestTaskContext: Codable {
+struct TestAction: Codable {
 	var testDescription = "TEST CONTEXT"
-	func encode() -> String {
-		let data = try! JSONEncoder().encode(self)
-		return String(data: data, encoding: .utf8)!
+	func encode() -> Data {
+		return try! JSONEncoder().encode(self)
 	}
-
+	
+	static func decode(from data: Data) -> TestAction{
+		let action = try! JSONDecoder().decode(TestAction.self, from: data)
+		return action
+	}
 }
 
 struct TestTaskRunner: TaskRunner {
-	func run(_ taskRecord: TaskManager.TaskRecord, completion: @escaping (TaskManager.TaskRecord) -> Void) {
-		print("running task \(taskRecord)")
-		completion(taskRecord)
+	func run(task: TaskManager.Task, completion: @escaping (Result<Bool, Error>) -> Void) {
+		print("TestTaskRunner running task \(task)")
+		let testAction = TestAction.decode(from: task.context)
+		print("Inner action = \(testAction.testDescription)")
+		completion(.success(true))
 	}
 	
 }
